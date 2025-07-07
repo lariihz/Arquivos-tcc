5980b9837eb5ec851893fc8815f75ceda1cd568f
Larissa Evelyn Marques <larissae1201@gmail.com>
2025-07-07 12:02:16 -0300

    Delete src directory

--- src/main/java/com/example/tcc_almoxarifado/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.tcc_almoxarifado.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.tcc_almoxarifado.model.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {}
